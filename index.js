47e1ace64434de16f0726c3942fa9c73224ec78b
vadim11041996 <vbraduleac@pentalog.com>
2019-05-19 21:45:02 +0300

    create function

---
+++ index.js
@@ -0,0 +1,19 @@
+function sum (){
+
+  const array = [3, 4, 5];
+
+  let suma = 0;
+
+  debugger
+  for (let i = 0; i < array.length; i++){
+
+    suma += array[i];
+
+  }
+
+  console.log(suma);
+
+}
+
+
+sum ()

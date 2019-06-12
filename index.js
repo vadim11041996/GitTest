d1ccba9b6de2aaa26b6b8dfd0de5b1e49a3123c9
vadim11041996 <vadim11041996@hotmail.com>
2019-06-12 19:39:05 +0300

    functie cu parametru

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const myVariable = "vadim";
 const yourVariable = "adelina";
 
 console.log(myVariable + yourVariable);
+
+// functie cu parametru
+function afisareNume(name) {
+  console.log("Numele meu este " + name);
+}
+
+afisareNume("Ileana");
+afisareNume("Ion");
+afisareNume();

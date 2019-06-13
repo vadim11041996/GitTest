fbf31dbca985752944d1f1da73a4a302f9ec1936
vadim11041996 <vadim11041996@hotmail.com>
2019-06-13 19:09:09 +0300

    first.html

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.warn("Resource not changed");
-console.error("File not found!");
+function display() {
+  const myFavouriteFood = "vrabiute";
+  console.log(myFavouriteFood);
+
+}
+
+console.log(myFavouriteFood);
+
+display();

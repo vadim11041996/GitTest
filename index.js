d1d22a474b261fd32196247aff6ae3b453c13ea9
vadim11041996 <vadim11041996@hotmail.com>
2019-06-16 14:24:18 +0300

    Tabele, forma

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-function display() {
-  const myFavouriteFood = "vrabiute";
-  console.log(myFavouriteFood);
+function submitForm(){
+    if(5>6){
+        console.log('Mai mare');
+    } else {
+        console.log("Nu e mai mare");
+    }
 
+    
+    return 5>6 && 7 > 9 ? console.log('Mai mare') : console.log('Nu e mai mare');
 }
 
-console.log(myFavouriteFood);
-
-display();
+submitForm()
